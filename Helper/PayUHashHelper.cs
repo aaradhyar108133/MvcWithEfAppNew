28ae25dd9ec61f0cf78aeae93700a3670b951fc0
Hp <Hp@HP-PC>
2025-05-13 10:08:58 +0530

    intigrate PayU money

---
+++ Helper/PayUHashHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CardPayment.Helper
+{
+    public class PayUHashHelper
+    {
+        public static string GenerateHash(string key, string txnId, string amount, string productInfo,
+       string firstName, string email, string salt)
+        {
+            string hashString = $"{key}|{txnId}|{amount}|{productInfo}|{firstName}|{email}|||||||||||{salt}";
+            using (var sha512 = SHA512.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(hashString);
+                byte[] hash = sha512.ComputeHash(bytes);
+                return BitConverter.ToString(hash).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
--- Models/PayUModel.cs
+++ Models/PayUModel.cs
@@ -2,20 +2,16 @@
 {
     public class PayUModel
     {
-        public  string Key { get; set; }
+        public string Key { get; set; }
         public string TxnId { get; set; }
         public string Amount { get; set; }
         public string ProductInfo { get; set; }
         public string FirstName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public string Surl { get; set; }
-        public string Furl { get; set; }
-        public string Udf1 { get; set; }
-        public string Udf2 { get; set; }
-        public string Udf3 { get; set; }
-        public string Udf4 { get; set; }
-        public string Udf5 { get; set; }
+        public string Surl { get; set; }  // Success URL
+        public string Furl { get; set; }  // Failure URL
         public string Hash { get; set; }
+        public string ServiceProvider { get; set; } = "payu_paisa";
     }
 }
