36cc5dbc4c926fc9956ec1da4ca017b3227a1d23
Hp <Hp@HP-PC>
2025-05-26 23:41:27 +0530

    new changes

--- Models/PayUModel.cs
+++ Models/PayUModel.cs
@@ -1,10 +1,18 @@
-﻿namespace CardPayment.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CardPayment.Models
 {
-    public class PayUModel
+    public class PayUModelRequest
     {
         public string Key { get; set; }
         public string TxnId { get; set; }
+
+        [JsonPropertyName("amount")]
         public string Amount { get; set; }
+
+        [JsonPropertyName ("cardId")]
+        public string CardId { get; set; }
+
         public string ProductInfo { get; set; }
         public string FirstName { get; set; }
         public string Email { get; set; }
@@ -13,5 +21,11 @@
         public string Furl { get; set; }  // Failure URL
         public string Hash { get; set; }
         public string ServiceProvider { get; set; } = "payu_paisa";
+    }
+
+    public class PayUModelResponse
+    {
+        public string message { get; set; }
+        public bool status { get; set; }
     }
 }
